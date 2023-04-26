40511f376f59df2fd712a6dbdad1eacc81904d95
Charles Severance <csev@umich.edu>
2023-04-26 06:12:00 -0400

    Addd search

--- read.php
+++ read.php
@@ -2,17 +2,22 @@
 
 // stdbuf -i0 -o0 -e0 bash yada.sh | tee -a /tmp/zap ; echo "Fini" >> /tmp/zap
 
-$start = $_GET['start'] ?? 0;
+$start = $_GET['start'] ?? 1;
+$limit = $_GET['limit'] ?? -1;
 $file = $_GET['file'] ?? null;
+if ( $start < 1 ) $start = 1;
 
 $lines = array();
 $fp = fopen($file, 'r');
-$pos = 0;
+$pos = 1;
+$shown = 0;
 
 while (($line = fgets($fp)) !== false) {
     if ( $pos >= $start ) {
         array_push($pos, rtrim($line));
         $lines[$pos] = $line;
+        $shown++;
+        if ( $limit > 0 && $shown >= $limit ) break;
     }
     $pos++;
 }
@@ -21,7 +26,7 @@ header('Content-Type: application/json');
 
 $retval = new \stdClass();
 $retval->lines = $lines;
-$retval->next = $pos;
+$retval->next = $pos+1;
 
 echo(json_encode($retval));
 
--- tail.php
+++ tail.php
@@ -1,5 +1,13 @@
 <html>
 <head>
+<?php
+$start = $_GET['start'] ?? 1;
+if ( $start < 1 ) $start = 1;
+$limit = $_GET['limit'] ?? 0;
+$file = $_GET['file'] ?? null;
+
+if ( !file_exists($file) ) die("File not found");
+?>
 <style>
 ol {
   font-family: monospace;
@@ -10,16 +18,41 @@ li::marker {
   font-size: 10px;
   color: grey;
 }
+main {
+  padding: 0;
+}
+header {
+  position: sticky;
+  top:0;
+  padding:1em;
+  background: lightblue;
+  text-align: center;
+}
+
+content > div {
+  height: 50px;
+}
 </style>
 </head>
 <body>
-<ol id="tail">
+<main>
+  <header>
+  File: <?= htmlentities($file) ?>
+
+<input type="text" id="search">
+<button onclick="doSearch(); return false;">Search</button>
+<button onclick="document.getElementById('search').value=''; doSearch(); return false;">Clear</button>
+  </header>
+  <content>
+  <ol id="tail" start="<?= $start ?>">
 </ol>
+</content>
+</main>
 <script>
 // stdbuf -i0 -o0 -e0 bash yada.sh | tee -a /tmp/zap ; echo "Fini" >> /tmp/zap
-var pos = 0;
+var pos = <?= $start ?>;
 function doTail() {
-  fetch('read.php?file=/tmp/zap&start='+pos)
+    fetch('read.php?file=<?= $file ?>&start='+pos+'&limit='+<?= $limit ?>)
     .then(response => response.json())
     .then(data => {
       console.log(data);
@@ -27,6 +60,14 @@ function doTail() {
           console.log('Key : ' + key + ', Value : ' + data.lines[key])
           var temp = document.createElement('li');
           temp.innerText = data.lines[key];
+
+          const str = document.getElementById('search').value;
+          // console.log('search', str);
+          if ( str.length > 0 && temp.innerText.includes(str) ) {
+              temp.style.backgroundColor = 'lightgrey';
+          } else {
+              temp.style.backgroundColor = 'white';
+          }
           document.getElementById("tail").appendChild(temp);
       })
       pos = data.next;
@@ -34,6 +75,21 @@ function doTail() {
     });
 }
 setTimeout(doTail, 500);
+
+function doSearch() {
+    const str = document.getElementById('search').value;
+    // console.log('search', str);
+    const parent = document.getElementById('tail');
+    Array.from(parent.children).forEach((child, index) => {
+        // console.log(index, child.innerText);
+        if ( str.length > 0 && child.innerText.includes(str) ) {
+            child.style.backgroundColor = 'lightgrey';
+        } else {
+            child.style.backgroundColor = 'white';
+        }
+    });
+}
+document.getElementById('search').value='';
 </script>
 </body>
 </html>
